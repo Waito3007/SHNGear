4eccd6802b73521a2bcbf1461d72561dd118b7c5
hoanghuy <dinhhoanghuy20044@gmail.com>
2025-02-25 14:38:51 +0700

    sửa

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
 import ProfilePage from "./pages/ProfilePage";
-=======
-import HomePage from "./Page/HomePage";
->>>>>>> 19e39e06573d551903db6d761f4471d35d8a8cf9
 
 const AppRoutes = [
   {
     index: true,
-<<<<<<< HEAD
+
     element: <Home />,
   },
   {
@@ -21,10 +17,6 @@ const AppRoutes = [
     path: "/Profile/:id",
     element: <ProfilePage />,
   },
-=======
-    element: <HomePage />,
-  },
->>>>>>> 19e39e06573d551903db6d761f4471d35d8a8cf9
 ];
 
 export default AppRoutes;
